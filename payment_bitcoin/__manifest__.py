6442378b7e79a4e43d02afdde02ffc4120d204d5
nileshsheliya <sheliyaniles@gmail.com>
2020-01-04 15:29:59 +0530

    Added payment_bitcoin and sale_payment module.

---
+++ payment_bitcoin/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Bitcoin Payment Acquirer',
+    'category': 'Hidden',
+    'summary': 'Payment Acquirer: Bitcoin Transfer Implementation',
+    'version': '1.2',
+    'description': """Bitcoin Transfer Payment Acquirer. The actual payment
+                   and wallet is kept separate. Bitcoin address need to be
+                   imported into this module. Each order gets an own dedicated
+                   Bitcoin address. The actual transaction needs to be
+                   registered manually.""",
+    'author': 'ZedeS Technologies',
+    'depends': ['payment', 'website_sale', 'sale_payment'],
+    'data': [
+        'views/bitcoin.xml',
+        'views/payment_bitcoin_templates.xml',
+        'views/templates.xml',
+        'views/cart.xml',
+        'data/payment_acquirer_data.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ sale_payment/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Nitrokey Sale Payment',
+    'version': '12.0.1.0.0',
+    'category': 'Website',
+    'author': 'ERP Harbor Consulting Services',
+    'website': 'http://www.erpharbor.com',
+    'summary': 'Nitrokey Sale Payment',
+    'description': """
+    This module adds the new fields which are available in v8 but not in v12.
+    and makes the other nitrokey custom module compatible with the v12.
+     """,
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
