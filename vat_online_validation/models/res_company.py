d54a810afdabb32f1d3557a032d355751b059705
jans23 <jans23@users.noreply.github.com>
2020-01-12 18:19:12 +0100

    terminology

--- vat_online_validation/models/res_company.py
+++ vat_online_validation/models/res_company.py
@@ -5,4 +5,4 @@ from odoo import models,fields
 class res_company(models.Model):
     _inherit = 'res.company'
     
-    must_validate_vat = fields.Boolean('Must validate Vat online?', help='If checked, system must validate vat number online and show error message if given vat number is not valid in partner/customer.')
+    must_validate_vat = fields.Boolean('Mandatory online verification?', help='If checked, system must validate vat number online and show error message if given vat number is not valid in partner/customer.')
