a19ac6491645bee858aab68ed85611759937ee57
nileshsheliya <sheliyaniles@gmail.com>
2020-01-15 14:06:50 +0530

    Added accounting_invoice_credit_note module.

---
+++ accounting_invoice_credit_note/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Nitrokey Invoice",
+    'description': """
+- Added Credti Note Smart button in invoice form
+    """,
+    'category': '',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_invoice_view.xml',
+        
+    ],
+    'images': [
+        
+    ],
+
+    'author': "initOS",
+    'website': "http://www.initos.com",
+}
