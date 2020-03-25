cabd98b8b93c59817d4da2fcfcc310e0a0faf4ef
david <david@nitrokey.com>
2020-03-25 09:50:21 +0100

    generated new README

--- invoice_export/__manifest__.py
+++ invoice_export/__manifest__.py
@@ -1,9 +1,11 @@
 {
-    'name': 'Nitrokey Invoice Export',
+    'name': 'Invoice Export',
     'version': '12.0.1.0.0',
-    'author': 'Nikunj Antala',
     'depends': ['sale', ], 
     'data': [
              'wizard/invoice_export_excel_view.xml',
             ],
+    'author': "Nitrokey GmbH, Odoo Community Association (OCA)",
+    'license': 'AGPL-3',
+    'website': "https://github.com/OCA/abandoned_carts",
 }
