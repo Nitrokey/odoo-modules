0f9aba96923181e11215962f8e773f91a904b106
nileshsheliya <sheliyaniles@gmail.com>
2020-04-08 17:18:50 +0530

    Fix code issue in vat_online_validation module.

--- vat_online_validation/models/res_company.py
+++ vat_online_validation/models/res_company.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    must_validate_vat = fields.Boolean('Mandatory online verification?',
+    must_validate_vat = fields.Boolean('Mandatory online Vat verification?',
                                        help='If enabled, the system validates '
                                             'VAT number online and shows error '
                                             'message if given VAT number of '
