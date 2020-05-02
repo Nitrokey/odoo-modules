98a240e327ae98a352f7d716633f8d94e45138dd
jans23 <jan@suhr.org>
2020-05-02 07:26:21 +0200

    vat_online_validation: terminology change

--- vat_online_validation/models/res_config_settings.py
+++ vat_online_validation/models/res_config_settings.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    must_validate_vat = fields.Boolean('Mandatory online VAT ID verification?',
+    must_validate_vat = fields.Boolean('Mandatory online VAT number verification?',
                                        related='company_id.must_validate_vat',
                                        readonly=False,
                                        help='If enabled, the system validates '
