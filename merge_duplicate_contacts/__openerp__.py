e0e98024069bf27202205d5154699966d79fd9e2
nileshsheliya <sheliyaniles@gmail.com>
2020-05-07 08:47:18 +0530

    Added merge_duplicate_contacts module.

---
+++ merge_duplicate_contacts/__openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Merge Duplicate Contacts',
+    'category': 'Other',
+    'version': '0.1',
+    'author': 'Nitrokey GmbH',
+    'description': """""",
+    'sequence': '1',
+    'depends': ['sale','crm'],
+    'data': [
+        'wizard/merge_contact_view.xml',
+    ],
+}
---
+++ merge_duplicate_contacts/wizard/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from . import merge_contact
