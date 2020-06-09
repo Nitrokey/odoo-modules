18a831ee72ee892d4a913bd6e3483d9a142526e4
Sudhir Arya <sudhir@erpharbor.com>
2020-06-09 15:27:57 +0530

    [ADD] Added the new module partner_delivery_link.

---
+++ partner_delivery_link/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Partner Delivery Link',
+    'version': '12.0.1.0.0',
+    'category': 'Website',
+    'author': 'ERP Harbor Consulting Services',
+    'website': 'http://www.erpharbor.com',
+    'summary': 'Partner Delivery Link',
+    'description': """
+Partner Delivery Link
+     """,
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+    'installable': True,
+}
