96e1f43bedd5696f26ba12f3ac1735ca2d3c6e53
Sudhir Arya <sudhir@erpharbor.com>
2020-07-28 13:36:49 +0530

    [ADD] Added the new module mass_mailing_double_opt_in for the newsletter subscription confirmation.

---
+++ mass_mailing_double_opt_in/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Mass Mailing Double opt-in',
+    'version': '12.0.1.0.0',
+    'category': 'Website',
+    'author': 'ERP Harbor Consulting Services',
+    'website': 'http://www.erpharbor.com',
+    'summary': 'Mass Mailing Double opt-in',
+    'description': """
+Mass Mailing Double opt-in
+     """,
+    'depends': [
+        'website_mass_mailing',
+    ],
+    'data': [
+        'data/mail_template.xml',
+        'views/subscribe_confirmation_template.xml',
+    ],
+    'installable': True,
+}
