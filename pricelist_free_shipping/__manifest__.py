d347a00e42953417baee8d9e9c463186dfa0404c
nikunj <nikunj.antala@gmail.com>
2020-09-08 11:33:30 +0530

    Added changes in pricelist_free_shipping module.

--- pricelist_free_shipping/__manifest__.py
+++ pricelist_free_shipping/__manifest__.py
@@ -7,7 +7,8 @@
     'author': 'Nitrokey GmbH',
     'depends': ['website_sale'],
     'data': [
-        'views/product_pricelist_views.xml'
+        'views/product_pricelist_views.xml',
+        'views/delivery_carrier_view.xml',
     ],
     'installable': True,
     'auto_install': False,
--- pricelist_free_shipping/models/delivery_carrier.py
+++ pricelist_free_shipping/models/delivery_carrier.py
@@ -1,28 +1,36 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+from odoo import models, fields, api
 
 class DeliveryCarrier(models.Model):
     _inherit = 'delivery.carrier'
 
+    is_free_for_promotion = fields.Boolean('Is Free for Promotion')
+    
+    @api.model
+    def _setup_free_delivery_methods(self):
+        del_method = self.sudo().env.ref('nitrokey_delivery_setup.carrier_letter_unregistered_grid_germany',False)
+        delivery_methods = self.browse()
+        if del_method:
+            delivery_methods += del_method
+        del_method = self.sudo().env.ref('nitrokey_delivery_setup.carrier_letter_registered_grid_germany',False)
+        if del_method:
+            delivery_methods += del_method
+            
+        del_method = self.sudo().env.ref('nitrokey_delivery_setup.carrier_letter_registered_grid_usa',False)
+        if del_method:
+            delivery_methods += del_method
+            
+        del_method = self.sudo().env.ref('nitrokey_delivery_setup.carrier_letter_registered_grid_world',False)
+        if del_method:
+            delivery_methods += del_method
+            
+        if len(delivery_methods):
+            delivery_methods.write({'is_free_for_promotion' : True})
+                
     def rate_shipment(self, order):
         res = super(DeliveryCarrier, self).rate_shipment(order)
-        del_method = self.sudo().env.ref('nitrokey_delivery_setup.carrier_letter_unregistered_grid_germany',False)
-        method_matched = False
-        if del_method and del_method.id == self.id:
-            method_matched = True
-        if not method_matched:
-            del_method = self.sudo().env.ref('nitrokey_delivery_setup.carrier_letter_registered_grid_germany',False)
-            if del_method and del_method.id == self.id:
-                method_matched = True
-        if not method_matched:
-            del_method = self.sudo().env.ref('nitrokey_delivery_setup.carrier_letter_registered_grid_usa',False)
-            if del_method and del_method.id == self.id:
-                method_matched = True
-        if not method_matched:
-            del_method = self.sudo().env.ref('nitrokey_delivery_setup.carrier_letter_registered_grid_world',False)
-            if del_method and del_method.id == self.id:
-                method_matched = True
-        if order.pricelist_id and method_matched:
+        
+        if order.pricelist_id and self.is_free_for_promotion:
             free_ship = True
             for product in [line.product_id for line in order.order_line.filtered(lambda x: not x.is_delivery)]:
                 item = self.env['product.pricelist.item'].search([('product_tmpl_id','=', product.product_tmpl_id.id), ('pricelist_id', '=', order.pricelist_id.id)], limit = 1)
