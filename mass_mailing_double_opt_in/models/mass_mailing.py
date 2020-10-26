7413da820d5ba4a77513c11d831c35a4766d3374
Sudhir Arya <sudhir@erpharbor.com>
2020-10-26 14:41:05 +0530

    [IMP] Improve the code for sending the mails related to newsletter subscription.

--- mass_mailing_double_opt_in/models/mass_mailing.py
+++ mass_mailing_double_opt_in/models/mass_mailing.py
@@ -6,3 +6,4 @@ class MassMailingContactListRel(models.Model):
     _inherit = 'mail.mass_mailing.list_contact_rel'
 
     access_token = fields.Char('Access Token', copy=False)
+    mail_language = fields.Char('Mail Language')
