dff745a21de6137d0d62dc13e71d2c72837a10c5
Sudhir Arya <sudhir@erpharbor.com>
2020-11-06 19:50:31 +0530

    [ADD] Added the new module partner_activity_overview to post the messages into the partner.

---
+++ partner_activity_overview/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Partner Activity Overview',
+    'author': 'ERP Harbor Consulting Services',
+    'category': 'Sales',
+    'summary': 'Partner Activity Overview',
+    'website': 'http://www.erpharbor.com',
+    'version': '12.0.1.0.0',
+    'description': """
+        Post the messages of different different objects (Sale, Purchase, Invoice, Lead, etc.) into the related partner's chatter box.
+    """,
+    'depends': [
+        'sale_crm', 'purchase_stock', 'nitrokey_helpdesk_mgmt', 'calendar', 'crm_phonecall',
+    ],
+    'installable': True,
+}
