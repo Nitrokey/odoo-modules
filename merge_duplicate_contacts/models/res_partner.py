243d3e59b1b4e478125d6b5dded7d6b0240988f9
nikunj <nikunj.antala@gmail.com>
2020-12-22 18:21:07 +0530

    Fix issue of code in merge_duplicate_contacts module.

--- merge_duplicate_contacts/models/res_partner.py
+++ merge_duplicate_contacts/models/res_partner.py
@@ -7,7 +7,8 @@ class ResPartner(models.Model):
     
     @api.multi
     def name_get(self):
-        if self._context.get('only_show_customer_id'):
+        ctx = self._context.copy() or {}
+        if ctx.get('only_show_customer_id') and 'show_address' not in ctx and 'show_address_only' not in ctx and 'show_email' not in ctx and 'html_format' not in ctx:
             res = []
             for record in self:
                 res.append((record.id, str(record.id)))
