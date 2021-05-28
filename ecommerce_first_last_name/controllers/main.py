25e7c50747a53aff28cc0e387039057b52d4c7b0
nikunj <nikunj.antala@gmail.com>
2021-05-28 14:30:12 +0530

    Fix issue in ecommerce_first_last_name module.

--- ecommerce_first_last_name/controllers/main.py
+++ ecommerce_first_last_name/controllers/main.py
@@ -8,8 +8,10 @@ _logger = logging.getLogger(__name__)
 class WebsiteSaleFirstLastname(WebsiteSale):
     def values_postprocess(self, order, mode, values, errors, error_msg):
         last_name = values.get('last_name')
-        if last_name:
-            values['name'] = values['name']+' '+last_name
+        if last_name and values.get('name'):
+            values['name'] = values.get('name')+' '+last_name
+        elif last_name:
+            values['name'] = last_name
         company_type = values.get('company_type')
         new_values, errors, error_msg = super(WebsiteSaleFirstLastname, self).values_postprocess(order, mode, values, errors, error_msg)
         if company_type:
