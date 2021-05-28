14af58e62adfaa076823ce91fc4ca4a1dbfa30fc
nikunj <nikunj.antala@gmail.com>
2021-05-28 14:26:43 +0530

    Fix issue in ecommerce_first_last_name module.

--- ecommerce_first_last_name/controllers/main.py
+++ ecommerce_first_last_name/controllers/main.py
@@ -1,45 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from odoo.http import request
-from odoo.addons.sale.controllers.product_configurator import ProductConfiguratorController
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 _logger = logging.getLogger(__name__)
 
-class WebsiteSale(ProductConfiguratorController):
+class WebsiteSaleFirstLastname(WebsiteSale):
     def values_postprocess(self, order, mode, values, errors, error_msg):
-        new_values = {}
-        authorized_fields = request.env['ir.model']._get('res.partner')._get_form_writable_fields()
-        for k, v in values.items():
-            # don't drop empty value, it could be a field to reset
-            if k in authorized_fields and v is not None:
-                new_values[k] = v
-            elif k == 'last_name':
-                new_values['name'] = new_values.get('name') + ' ' + v
-            elif k == 'company_type':
-                new_values['company_type'] = v
-            else:  # DEBUG ONLY
-                if k not in ('field_required', 'partner_id', 'callback', 'submitted'): # classic case
-                    _logger.debug("website_sale postprocess: %s value has been dropped (empty or not writable)" % k)
-
-        new_values['customer'] = True
-        new_values['team_id'] = request.website.salesteam_id and request.website.salesteam_id.id
-        new_values['user_id'] = request.website.salesperson_id and request.website.salesperson_id.id
-
-        if request.website.specific_user_account:
-            new_values['website_id'] = request.website.id
-
-        if mode[0] == 'new':
-            new_values['company_id'] = request.website.company_id.id
-
-        lang = request.lang if request.lang in request.website.mapped('language_ids.code') else None
-        if lang:
-            new_values['lang'] = lang
-        if mode == ('edit', 'billing') and order.partner_id.type == 'contact':
-            new_values['type'] = 'other'
-        if mode[1] == 'shipping':
-            new_values['parent_id'] = order.partner_id.commercial_partner_id.id
-            new_values['type'] = 'delivery'
-
+        last_name = values.get('last_name')
+        if last_name:
+            values['name'] = values['name']+' '+last_name
+        company_type = values.get('company_type')
+        new_values, errors, error_msg = super(WebsiteSaleFirstLastname, self).values_postprocess(order, mode, values, errors, error_msg)
+        if company_type:
+            new_values['company_type'] = company_type
         return new_values, errors, error_msg
     
