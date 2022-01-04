e36740fa550b2abcb9e0de093ff5715ded0388a6
nikunj <nikunj.antala@gmail.com>
2022-01-04 13:59:51 +0530

    Added product_label_image module.

---
+++ product_label_image/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Label Image',
+    'category': '',
+    'version': '12.0.0.1',
+    'author': '',
+    'description': """Product barcode print with product image""",
+    'sequence': '1',
+    'depends': ['stock'],
+    'data': [
+        'reports/product_template_report.xml',
+        ],
+}
