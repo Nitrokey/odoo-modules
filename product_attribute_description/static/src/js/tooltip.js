fc1a73fa94aa22893f33a075f36b8e50c765c478
nikunj <nikunj.antala@gmail.com>
2022-01-18 17:16:41 +0530

    Fix tooltip showing issue in product_attribute_description module.

--- product_attribute_description/static/src/js/tooltip.js
+++ product_attribute_description/static/src/js/tooltip.js
@@ -1,6 +1,8 @@
 odoo.define('product_attribute_description.product_attribute_description', function (require) {
 'use strict';
 	
+	$('[data-toggle="tooltip"]').tooltip('show').tooltip('hide');
+	
 	$('[data-toggle="tooltip"]').on({
 	  "click": function() {		
 	    $(this).tooltip('show');
