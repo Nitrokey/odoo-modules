ae97969f4661050186c16ca72d5136f9dec351a0
nikunj <nikunj.antala@gmail.com>
2022-02-09 14:50:13 +0530

    Fix tooltip style issue in product_attribute_description module.

--- product_attribute_description/static/src/js/tooltip.js
+++ product_attribute_description/static/src/js/tooltip.js
@@ -1,15 +1,18 @@
 odoo.define('product_attribute_description.product_attribute_description', function (require) {
 'use strict';
 	
-	$('[data-toggle="tooltip"]').tooltip('show').tooltip('hide');
-	
-	$('[data-toggle="tooltip"]').on({
-	  "click": function() {		
-	    $(this).tooltip('show');
-	  }
-	});
-	
-	
+//	$('[data-toggle="tooltip"]').tooltip('show').tooltip('hide');
+
+//        $('#description_tooltip').on({
+//          "click": function() {
+//             $(this).tooltip('show');
+//          }
+//        });
+
+        $( "#description_tooltip").mouseover(function(){
+            $(this).tooltip('show');
+        });
+
 });
 
 
