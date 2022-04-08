2a5af9ef790ec54caf4d15721d710bcabbe807c6
nikunj <nikunj.antala@gmail.com>
2022-04-08 11:44:47 +0530

    Added partner_user_status module.

---
+++ partner_user_status/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Partner User Status',
+    'version': '12.0.1.0',
+    'summary': '',
+    'description': """Added Users field in Partner form view""",
+    'category': 'Tools',
+    'author': "initOS GmbH",
+    'license': 'AGPL-3',
+    'website': "http://www.initos.com",
+    'depends': ['base'],
+    'data': [
+        'view/partner.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
