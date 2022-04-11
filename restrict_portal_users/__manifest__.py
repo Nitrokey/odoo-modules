facf71d61c5a412a59b54d28a2b93bbd07a978ba
nikunj <nikunj.antala@gmail.com>
2022-04-11 19:48:54 +0530

    Added restrict_portal_users module.

---
+++ restrict_portal_users/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Restrict Portal Users",
+
+    'summary': """
+        Restrict the portal user""",
+
+    'description': """
+       When user do following action in odoo, it only shows internal users contacts in dropdown.
+            Chatter mentioning (@username)
+            Project task assignment
+            Helpdesk ticket assignments
+            Sales order assignments (Salesperson)
+            Invoicing, Salesperson
+            Purchase Representative
+            Manufacturing Responsible
+
+    """,
+
+    # Author
+
+    'author': "Nitrokey GmbH",
+    'license': 'AGPL-3',
+    'website': "https://www.nitrokey.com",
+
+    'version': '12.0.1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['purchase', 'sale_management', 'helpdesk_mgmt', 'project','mrp'],
+
+    # always loaded
+    'data': [
+        'views/purchase_order.xml',
+        'views/account_invoice.xml',
+        'views/mrp_production.xml',
+        'views/sale_order.xml',
+        'views/helpdesk_ticket.xml',
+        'views/project_task.xml',
+    ],
+
+}
---
+++ restrict_portal_users/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import helpdesk_ticket
+from . import res_partner
