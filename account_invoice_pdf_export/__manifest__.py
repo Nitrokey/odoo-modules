8e9a64db8ebbaab8bed42c20006b0b067a3799e9
dsolanki <dhara.solanki@initos.com>
2022-04-20 17:01:17 +0530

    [MOD] Modified manifest file as per suggestion

--- account_invoice_pdf_export/__manifest__.py
+++ account_invoice_pdf_export/__manifest__.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright 2016 initOS GmbH
+#    Copyright 2022 initOS GmbH
 #
 ##############################################################################
 {
     'name': "Export Invoices",
     'description': """Export multiple invoices PDF in Zip 
+    Allows User To Select From Date and To Date For Export Invoices in ZIP Format For Selected Invoice status.
+    For this Go To The Invoice > Click On Reporting Menu > Select the Export Invoices.
     """,
     'category': '',
     'version': '15.0.1.0.0',
@@ -16,9 +18,8 @@
     'data': [
         'security/ir.model.access.csv',
         'wizard/invoice_pdf_export_view.xml',
-        
     ],
     'author': "Nitrokey GmbH",
     'license': 'AGPL-3',
-    'website': "http://www.initos.com",
+    'website': "http://www.nitrokey.com",
 }
