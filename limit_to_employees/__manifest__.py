1475aa8cd41207f92462994b86a8a89b2b1f7828
Nikunj <nantala>
2022-05-06 19:58:54 +0530

    [MIG] limit_to_employees: Migration to 15.0

---
+++ limit_to_employees/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Limit To Employees",
+
+    'summary': """
+        Limit To Employees""",
+
+    'description': """
+       When user do following action in odoo, it only shows internal users contacts in dropdown.
+            Chatter mentioning (@username)
+            Project task assignment
+            Helpdesk ticket assignments
+            Sales order assignments (Salesperson)
+            Invoicing, Salesperson
+            Purchase Representative
+            Manufacturing Responsible
+
+    """,
+
+    # Author
+
+    'author': "Nitrokey GmbH",
+    'license': 'AGPL-3',
+    'website': "https://www.nitrokey.com",
+
+    'version': '15.0.1.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['helpdesk_mgmt', 'account'],
+
+    # always loaded
+    'data': [
+        'views/account_invoice.xml',
+    ],
+
+}
