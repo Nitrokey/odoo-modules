db3654f24a7b75ad3a253e0ad2c8830f359ed6ff
Nikunj <nantala>
2022-05-06 18:38:40 +0530

    [MIG] partner_user_status: Migration to 15.0

---
+++ partner_user_status/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Partner User Status',
+    'version': '15.0.1.0',
+    'summary': '',
+    'description': """Added Users field in Partner form view""",
+    'category': 'Tools',
+    'author': "Nitrokey GmbH",
+    'license': 'AGPL-3',
+    'website': "http://www.nitrokey.com",
+    'depends': ['base'],
+    'data': [
+        'view/partner.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
