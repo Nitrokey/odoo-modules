a593ffd57f47eacc400f4fb6c19653b6be44be14
nikunj <nikunj.antala@gmail.com>
2022-05-07 09:56:32 +0530

    Changed label of hide_accessory_product field in product.template model in ecommerce_hide_product module.

--- ecommerce_hide_product/models/product.py
+++ ecommerce_hide_product/models/product.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    hide_accessory_product = fields.Boolean(string='Hide Accessory Product')
+    hide_accessory_product = fields.Boolean(string='Hide from shop overview')
