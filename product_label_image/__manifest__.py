7dd754b14b25cfeafca11becf83a86e0ad1cb88c
Nikunj <nantala>
2022-05-07 19:33:09 +0530

    [MIG] product_label_image: Migration to 15.0

---
+++ product_label_image/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Label Image',
+    'category': '',
+    'version': '15.0.0.1',
+    'author': 'Nitrokey GmbH',
+    'website': "http://www.nitrokey.com",
+    'description': """Product barcode print with product image""",
+    'depends': ['product'],
+    'data': [
+        'reports/product_template_report.xml',
+        ],
+}
