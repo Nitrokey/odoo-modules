f84a3f3f50b3a7ba2d60e7f5145d2fe01768739b
nikunj <nikunj.antala@gmail.com>
2022-05-31 20:02:50 +0530

    Added product_mandatory_products module.

---
+++ product_mandatory_products/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Product Mandatory Products',
+    'version': '12.0.1.0.1',
+    'category': 'Product',
+    'summary': 'This module allows you to set a product as a Pack',
+    'license': 'AGPL-3',
+    'depends': [
+        'product','website_sale',
+    ],
+    'data': [
+        'views/product_template_view.xml',
+        'views/sale_product_configurator_templates.xml',
+        'views/templates.xml',
+            ],
+    # Author
+    'author': "Nitrokey GmbH",
+    'website': "http://www.nitrokey.com",
+    'maintainer': 'Nitrokey GmbH',
+    
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
