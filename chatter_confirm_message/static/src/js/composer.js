f4c85ff80306e0c6875f1436f924baf5b517ccaa
nikunj <nikunj.antala@gmail.com>
2022-06-30 16:45:09 +0530

    Improvement in chatter_confirm_message module.

--- chatter_confirm_message/static/src/js/composer.js
+++ chatter_confirm_message/static/src/js/composer.js
@@ -2,11 +2,14 @@ odoo.define('chatter_confirm_message.composer.Basic', function (require) {
 "use strict";
 var BasicComposer = require('mail.composer.Basic');
 var Dialog = require('web.Dialog');
+var translation = require('web.translation');
+var _t = translation._t;
 const rpc = require('web.rpc');
 
     BasicComposer.include({
     _sendMessage: function () {
         var _super =  this._super.bind(this);
+        if (this.__parentedParent.record &&	this.options.isLog == false){
         var follower_ids =  this.__parentedParent.record.data.message_follower_ids.res_ids
         if (follower_ids.length != 0){
 	         var self = this;
@@ -25,22 +28,24 @@ const rpc = require('web.rpc');
         	var dialog = new Dialog(self, {
                     title:('Confirmation For Chatter'),
                     size: 'medium',
-                    $content:  "<p>Confirm To Send Message?</p>",
+                    $content: (_t("<p>Your message will be sent to external partners (e.g. customers).</p>")),
                     buttons : [
-                    {text: ('Close'), close: true, classes: 'btn-primary'},
-                    {text: ('Confirm'), click: function () {
+                    {text: (_t('Send')), click: function () {
 
                         _super();
                          dialog.close()
                     },
-                        classes: 'btn-primary'}
+                        classes: 'btn-primary'},
+                    {text: (_t('Cancel')), close: true, classes: 'btn-primary'}
                 ]
                 });
         }
         else{
          _super();
         }
-
+    }
+    else{ _super();
+    }
     },
 
 });
