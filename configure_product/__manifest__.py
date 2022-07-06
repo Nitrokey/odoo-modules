e97866af6f07bd1eb3ff773d9dbf5229bb4fde40
Nikunj <nikunj.antala@gmail.com>
2022-07-06 14:12:43 +0530

    Reformatted code of configure_product module.

--- configure_product/__manifest__.py
+++ configure_product/__manifest__.py
@@ -1,17 +1,18 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "Configure Product",
-    'category': 'Hidden',
-    'description': """The system will display Configure Product wizard after Adding a product to the sale order and purchase order form.
+    "name": "Configure Product",
+    "category": "Hidden",
+    "summary": """The system will display Configure Product wizard after Adding a product to the sale order and purchase order form.
     """,
-    'version': '15.0.1.0.0',
-    'depends': [
-        'sale_product_configurator', 'purchase', 'sale_management', 
+    "version": "15.0.1.0.0",
+    "depends": [
+        "sale_product_configurator",
+        "purchase",
+        "sale_management",
     ],
-    'data': [
-        'views/purchase_view.xml',
+    "data": [
+        "views/purchase_view.xml",
     ],
-    'author': "Nitrokey GmbH",
-    'license': 'LGPL-3',
-    'website': 'www.nitrokey.com',
+    "author": "Nitrokey GmbH",
+    "license": "LGPL-3",
+    "website": "https://github.com/OCA/server-tools",
 }
--- configure_product/models/product_attribute.py
+++ configure_product/models/product_attribute.py
@@ -1,6 +1,12 @@
-from odoo import fields
+from odoo import fields, models
+
 
 class ProductAttributeCustomValue(models.Model):
     _inherit = "product.attribute.custom.value"
 
-    purchase_order_line_id = fields.Many2one('purchase.order.line', string="Purchase Order Line", required=True, ondelete='cascade')
+    purchase_order_line_id = fields.Many2one(
+        "purchase.order.line",
+        string="Purchase Order Line",
+        required=True,
+        ondelete="cascade",
+    )
