c282242053bb6ac31363b8326d34ada9bfa2e2eb
Nikunj <nikunj.antala@gmail.com>
2022-07-06 12:32:32 +0530

    Reformatted code in chatter_confirm_message.

--- chatter_confirm_message/__manifest__.py
+++ chatter_confirm_message/__manifest__.py
@@ -5,21 +5,19 @@
 This module will show confirmation dialog while sending message from chatter when any one follower of the record is not internal user.
     """,
     # Author
-    'author': "Nitrokey GmbH",
-    'website': "http://www.nitrokey.com",
-    'maintainer': 'Nitrokey GmbH',
-
-    "depends": ["mail", 'base'],
-    "data": [
-
-    ],
+    "author": "Nitrokey GmbH",
+    "website": "https://github.com/OCA/server-tools",
+    "maintainer": "Nitrokey GmbH",
+    "depends": ["mail", "base"],
+    "data": [],
     "installable": True,
-    'assets': {
-        'web.assets_backend': [
-            ('replace', 'mail/static/src/components/composer/composer.js',
-             'chatter_confirm_message/static/src/components/composer/composer.js'),
-
+    "assets": {
+        "web.assets_backend": [
+            (
+                "replace",
+                "mail/static/src/components/composer/composer.js",
+                "chatter_confirm_message/static/src/components/composer/composer.js",
+            ),
         ],
-
-    }
+    },
 }
--- chatter_confirm_message/models/res_partner.py
+++ chatter_confirm_message/models/res_partner.py
@@ -1,25 +1,26 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
 import re
 
+from odoo import api, models
+
+
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     @api.model
     def check_users(self, follower_ids):
-        mail=[]
+        mail = []
         for i in follower_ids:
 
-            str_id = re.search('\d+', i)
+            str_id = re.search(r"\d+", i)
             mail.append(int(str_id.group()))
 
-        followers = self.env['mail.followers'].browse(mail)
+        followers = self.env["mail.followers"].browse(mail)
         for follower in followers:
             for partner in follower.partner_id:
                 if not partner.user_ids:
                     return True
                 for user in partner.user_ids:
-                    if user.has_group('base.group_portal'):
+                    if user.has_group("base.group_portal"):
                         return True
 
         return False
