6457c988eb5c490db32ae1c6b989014a537963a7
nikunj <nikunj.antala@gmail.com>
2022-08-29 16:50:40 +0530

    Changed external id while sending report to printer

--- delivery_automatic_print/models/report_print.py
+++ delivery_automatic_print/models/report_print.py
@@ -9,7 +9,7 @@ class StockPicking(models.Model):
         for pickings in self:
             if pickings.picking_type_id.code == 'outgoing':
                 if pickings.ups_image or pickings.label_de_attach_id:
-                    pdf = self.env.ref('carrier_shipping_label_template.report_shipping_label').sudo().render_qweb_pdf(
+                    pdf = self.env.ref('carrier_shipping_label_template.action_report_shipping_label').sudo().render_qweb_pdf(
                         [self.id])[0]
                 pdf = self.env.ref('stock.action_report_picking').sudo().render_qweb_pdf(
                     [self.id])[0]
