f4848940926077281bdf9fe854fe16e099c0cbfe
nikunj <nikunj.antala@gmail.com>
2022-08-30 16:25:33 +0530

    Inherit render_qweb_pdf method instead of override in carrier_shipping_label_template module.

--- carrier_shipping_label_template/models/report.py
+++ carrier_shipping_label_template/models/report.py
@@ -1,12 +1,4 @@
-from collections import OrderedDict
-from odoo import models, api, tools, _
-from odoo.tools import config
-
-from odoo.sql_db import TestCursor
-from odoo.exceptions import UserError
-import logging
-
-_logger = logging.getLogger(__name__)
+from odoo import models, api
 
 
 class IrActionsReportReportlab(models.Model):
@@ -14,111 +6,49 @@ class IrActionsReportReportlab(models.Model):
 
     @api.multi
     def render_qweb_pdf(self, res_ids=None, data=None):
+        if  self.report_name in ['carrier_shipping_label_template.report_shipping_label']:
+            self = self.with_context(shipping_label_res_ids=res_ids)
+        return super(IrActionsReportReportlab, self).render_qweb_pdf(res_ids, data)
+
+    @api.model
+    def render_qweb_html(self, docids, data=None):
+        """This method generates and returns html version of a report.
+        """
         if (self.report_name not in ['carrier_shipping_label_template.report_shipping_label']) \
-                or not res_ids:
-            return super().render_qweb_pdf(res_ids, data)
-
-        if not data:
-            data = {}
-        data.setdefault('report_type', 'pdf')
-
-        # remove editor feature in pdf generation
-        data.update(enable_editor=False)
-
-        # In case of test environment without enough workers to perform calls to wkhtmltopdf,
-        # fallback to render_html.
-        if (tools.config['test_enable'] or tools.config['test_file']) and not self.env.context.get(
-                'force_report_rendering'):
-            return self.render_qweb_html(res_ids, data=data)
-
-        # As the assets are generated during the same transaction as the rendering of the
-        # templates calling them, there is a scenario where the assets are unreachable: when
-        # you make a request to read the assets while the transaction creating them is not done.
-        # Indeed, when you make an asset request, the controller has to read the `ir.attachment`
-        # table.
-        # This scenario happens when you want to print a PDF report for the first time, as the
-        # assets are not in cache and must be generated. To workaround this issue, we manually
-        # commit the writes in the `ir.attachment` table. It is done thanks to a key in the context.
-        context = dict(self.env.context)
-        if not config['test_enable']:
-            context['commit_assetsbundle'] = True
-
-        # Disable the debug mode in the PDF rendering in order to not split the assets bundle
-        # into separated files to load. This is done because of an issue in wkhtmltopdf
-        # failing to load the CSS/Javascript resources in time.
-        # Without this, the header/footer of the reports randomly disapear
-        # because the resources files are not loaded in time.
-        # https://github.com/wkhtmltopdf/wkhtmltopdf/issues/2083
-        context['debug'] = False
-
-        # The test cursor prevents the use of another environnment while the current
-        # transaction is not finished, leading to a deadlock when the report requests
-        # an asset bundle during the execution of test scenarios. In this case, return
-        # the html version.
-        if isinstance(self.env.cr, TestCursor):
-            return self.with_context(context).render_qweb_html(res_ids, data=data)[0]
-
-        save_in_attachment = OrderedDict()
-        if res_ids:
-            # Dispatch the records by ones having an attachment and ones requesting a call to
-            # wkhtmltopdf.
-            Model = self.env[self.model]
-            record_ids = Model.browse(res_ids)
-            wk_record_ids = Model
-            # change by Synodica Solutions
-            if self.attachment:
-                for record_id in record_ids:
-                    attachment_id = self.retrieve_attachment(record_id)
-                    if not attachment_id:
-                        attachment_id = record_id.label_de_attach_id
-                    if attachment_id:
-                        save_in_attachment[record_id.id] = attachment_id
-                    if not self.attachment_use or not attachment_id:
-                        wk_record_ids += record_id
-            # change by Synodica Solutions
+                or not docids:
+            return super(IrActionsReportReportlab, self).render_qweb_html(docids, data)
+        new_doc_ids = []
+        for picking in self.env['stock.picking'].browse(docids):
+            if picking.label_de_attach_id:
+                continue
             else:
-                for record_id in record_ids:
-                    attachment_id = record_id.label_de_attach_id
-                    if attachment_id:
-                        save_in_attachment[record_id.id] = attachment_id
-                    else:
-                        wk_record_ids += record_id
+                new_doc_ids.append(picking.id)
+        return super(IrActionsReportReportlab, self).render_qweb_html(new_doc_ids, data)
+
+    @api.multi
+    def _post_pdf(self, save_in_attachment, pdf_content=None, res_ids=None):
+        if self.report_name not in ['carrier_shipping_label_template.report_shipping_label']:
+            return super(IrActionsReportReportlab, self)._post_pdf(save_in_attachment, pdf_content, res_ids)
+
+        shipping_label_res_ids = self._context.get('shipping_label_res_ids')
+        if shipping_label_res_ids:
+            Model = self.env[self.model]
+            records = Model.browse(shipping_label_res_ids)
+            wk_record_ids = Model
+            for record in records:
+                attachment_id = record.label_de_attach_id
+                if attachment_id:
+                    save_in_attachment[record.id] = attachment_id
+                else:
+                    wk_record_ids += record
             res_ids = wk_record_ids.ids
+            if not res_ids:
+                pdf_content = None
 
-        # A call to wkhtmltopdf is mandatory in 2 cases:
-        # - The report is not linked to a record.
-        # - The report is not fully present in attachments.
-        if save_in_attachment and not res_ids:
-            _logger.info('The PDF report has been generated from attachments.')
-            return self._post_pdf(save_in_attachment), 'pdf'
+        return super(IrActionsReportReportlab, self)._post_pdf(save_in_attachment, pdf_content, res_ids)
 
-        if self.get_wkhtmltopdf_state() == 'install':
-            # wkhtmltopdf is not installed
-            # the call should be catched before (cf /report/check_wkhtmltopdf) but
-            # if get_pdf is called manually (email template), the check could be
-            # bypassed
-            raise UserError(_("Unable to find Wkhtmltopdf on this system. The PDF can not be created."))
-
-        html = self.with_context(context).render_qweb_html(res_ids, data=data)[0]
-
-        # Ensure the current document is utf-8 encoded.
-        html = html.decode('utf-8')
-
-        bodies, html_ids, header, footer, specific_paperformat_args = self.with_context(context)._prepare_html(html)
-
-        if self.attachment and set(res_ids) != set(html_ids):
-            raise UserError(_("The report's template '%s' is wrong, please contact your administrator. \n\n"
-                              "Can not separate file to save as attachment because the report's template does not contains the attributes 'data-oe-model' and 'data-oe-id' on the div with 'article' classname.") % self.name)
-
-        pdf_content = self._run_wkhtmltopdf(
-            bodies,
-            header=header,
-            footer=footer,
-            landscape=context.get('landscape'),
-            specific_paperformat_args=specific_paperformat_args,
-            set_viewport_size=context.get('set_viewport_size'),
-        )
-        if res_ids:
-            _logger.info('The PDF report has been generated for records %s.' % (str(res_ids)))
-            return self._post_pdf(save_in_attachment, pdf_content=pdf_content, res_ids=html_ids), 'pdf'
-        return pdf_content, 'pdf'
+    @api.multi
+    def retrieve_attachment(self, record):
+        if self.report_name in ['carrier_shipping_label_template.report_shipping_label']:
+            return None
+        return super(IrActionsReportReportlab, self).retrieve_attachment(record)
