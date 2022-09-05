72cc882a173f49b8c49601cb4b09bd8b45bce0fc
Nikunj <nikunj.antala@gmail.com>
2022-09-05 18:15:04 +0530

    [Fix] Applied pre-commit command to fix code structure.

--- limit_to_employees/__manifest__.py
+++ limit_to_employees/__manifest__.py
@@ -1,31 +1,20 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "Limit To Employees",
-
-    'summary': """
-        Limit To Employees""",
-
-    'description': """
-       When user do following action in odoo, it only shows internal users contacts in dropdown.
-            Chatter mentioning (@username)
-            Helpdesk ticket assignments
-            Invoicing, Salesperson
+    "name": "Limit To Employees",
+    "summary": """
+When user do following action in odoo, it only shows internal users contacts in dropdown.
+  * Chatter mentioning (@username)
+  * Helpdesk ticket assignments
+  * Invoicing, Salesperson
     """,
-
     # Author
-
-    'author': "Nitrokey GmbH",
-    'license': 'AGPL-3',
-    'website': "https://www.nitrokey.com",
-
-    'version': '15.0.1.1',
-
+    "author": "Nitrokey GmbH",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/server-tools",
+    "version": "15.0.1.1.0",
     # any module necessary for this one to work correctly
-    'depends': ['helpdesk_mgmt', 'account'],
-
+    "depends": ["helpdesk_mgmt", "account"],
     # always loaded
-    'data': [
-        'views/account_invoice.xml',
+    "data": [
+        "views/account_invoice.xml",
     ],
-
 }
--- limit_to_employees/models/__init__.py
+++ limit_to_employees/models/__init__.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
 from . import helpdesk_ticket
 from . import res_partner
