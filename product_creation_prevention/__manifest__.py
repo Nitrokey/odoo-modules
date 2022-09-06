501bbbc58fca55dc83070b116bf4bb75d7e82b97
dsolanki <dhara.solanki@initos.com>
2022-09-06 11:08:31 +0530

    Rebase the code, modified as per feedback

--- product_creation_prevention/__manifest__.py
+++ product_creation_prevention/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': 'Product Creation Prevention',
     'version': '12.0.0.1',
@@ -11,11 +10,7 @@
         'purchase'
     ],
     'data': [
-        'views/account_view.xml',
-        'views/sale_view.xml',
-        'views/mrp_view.xml',
-        'views/picking_view.xml',
-        'views/purchase_view.xml',
+        'security/ir.model.access.csv',
         ],
     'installable': True,
 }
