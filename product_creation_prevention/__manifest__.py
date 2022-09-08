6269ee98551fd6545a5c37a7b0862dc92a21c485
dsolanki <dhara.solanki@initos.com>
2022-09-08 15:25:36 +0530

    Modified module description

--- product_creation_prevention/__manifest__.py
+++ product_creation_prevention/__manifest__.py
@@ -1,7 +1,9 @@
 {
     'name': 'Product Creation Prevention',
     'version': '12.0.0.1',
-    'description': """Product Creation Prevention""",
+    'description': """Product Creation Prevention
+    By adding access right we are preventing users creating a products 'on the fly'
+    """,
     'depends': [
         'account',
         'stock',
