6b47eb53020c68469293016a9ac84cdac137d346
dsolanki <dhara.solanki@initos.com>
2022-09-09 10:30:21 +0530

    Modified description and vesrion

--- product_creation_prevention/__manifest__.py
+++ product_creation_prevention/__manifest__.py
@@ -1,8 +1,8 @@
 {
     'name': 'Product Creation Prevention',
-    'version': '12.0.0.1',
+    'version': '12.0.1.0.0',
     'description': """Product Creation Prevention
-    By adding access right we are preventing users creating a products 'on the fly'
+    By adding access right we are preventing users to create products 'on the fly'
     """,
     'depends': [
         'account',
