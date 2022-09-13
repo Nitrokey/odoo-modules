0a6339a6f95371612766a7748674b03da4a8c19d
dsolanki <dhara.solanki@initos.com>
2022-09-13 14:41:58 +0530

    [15.0][ADD]crm_profiling

---
+++ crm_profiling/readme/DESCRIPTION.rst
@@ -0,0 +1,25 @@
+==============
+CRM Profiling
+==============
+
+This module allows users to perform segmentation within partners.
+
+It uses the profiles criteria from the earlier segmentation module and
+improve it. Thanks to the new concept of questionnaire. You can now regroup
+questions into a questionnaire and directly use it on a partner.
+
+It also has been merged with the earlier CRM & SRM segmentation tool
+because they were overlapping.
+
+** Note: ** this module is not compatible with the module segmentation, since
+it's the same which has been renamed.
+
+
+Authors
+~~~~~~~
+* Nitrokey GmbH
+
+Contributors
+~~~~~~~~~~~~
+
+* Dhara Solanki <dhara.solanki@initos.com>
---
+++ crm_profiling/wizard/open_questionnaire.py
@@ -0,0 +1,73 @@
+from odoo import _, api, fields, models
+
+
+class OpenQuestionnaireLine(models.TransientModel):
+    _name = "open.questionnaire.line"
+    _rec_name = "question_id"
+    _description = "Open Questionnaire Line"
+
+    question_id = fields.Many2one("crm_profiling.question", "Question", required=True)
+    answer_id = fields.Many2one("crm_profiling.answer", "Answer")
+    wizard_id = fields.Many2one("open.questionnaire", "Questionnaire")
+
+
+class OpenQuestionnaire(models.TransientModel):
+    _name = "open.questionnaire"
+    _description = "Open Questionnaire"
+
+    questionnaire_id = fields.Many2one(
+        "crm_profiling.questionnaire", "Questionnaire name"
+    )
+    question_ans_ids = fields.One2many(
+        "open.questionnaire.line", "wizard_id", "Question / Answers"
+    )
+
+    @api.model
+    def default_get(self, fields):
+        context = self._context.copy()
+        res = super(OpenQuestionnaire, self).default_get(fields)
+        questionnaire_id = context.get("questionnaire_id", False)
+        if questionnaire_id and "question_ans_ids" in fields:
+            query = """
+                SELECT question AS question_id
+                FROM profile_questionnaire_quest_rel
+                WHERE questionnaire = %s"""
+            self._cr.execute(query, (questionnaire_id,))
+            result = self._cr.dictfetchall()
+            final_data = []
+            for data in result:
+                final_data.append((0, 0, data))
+            res.update(question_ans_ids=final_data)
+        return res
+
+    def questionnaire_compute(self):
+        """Adds selected answers in partner form"""
+        model = self._context.get("active_model")
+        answers = []
+        if model == "res.partner":
+            for d in self.question_ans_ids:
+                if d.answer_id:
+                    answers.append(d.answer_id.id)
+            self.env[model]._questionnaire_compute(answers)
+        return {"type": "ir.actions.act_window_close"}
+
+    def build_form(self):
+        models_data = self.env["ir.model.data"]
+        result = models_data.check_object_reference(
+            "crm_profiling", "open_questionnaire_form"
+        )
+        res_id = models_data.browse(result[1]).id
+        context = dict(
+            self.env.context or {},
+            questionnaire_id=self.questionnaire_id.id,
+        )
+
+        return {
+            "name": _("Questionnaire"),
+            "view_mode": "form",
+            "res_model": "open.questionnaire",
+            "type": "ir.actions.act_window",
+            "views": [(res_id, "form")],
+            "target": "new",
+            "context": context,
+        }
