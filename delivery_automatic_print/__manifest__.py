2a96f60622f18dd8a7c93c5e631b637016d40b0e
Nikunj <nikunj.antala@gmail.com>
2022-09-24 11:43:11 +0530

    [15.0][MIG] delivery_automatic_print

---
+++ delivery_automatic_print/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Delivery Automatic Print",
+    "summary": "Print shipping label automatically",
+    "version": "15.0.1.0.0",
+    "depends": ["carrier_shipping_label_template", "stock"],
+    "data": [],
+    "auto_install": False,
+    "installable": True,
+    "author": "Nitrokey GmbH",
+    "license": "LGPL-3",
+    "website": "https://github.com/OCA/server-tools",
+    "maintainer": "Nitrokey GmbH",
+}
