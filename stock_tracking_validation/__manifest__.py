82e2ef04f40c82fb97a09b23013fd62e421f0668
dsolanki <dhara.solanki@initos.com>
2022-12-29 17:07:27 +0530

    [CHANGE] pre-commit required changes

--- stock_tracking_validation/__manifest__.py
+++ stock_tracking_validation/__manifest__.py
@@ -4,11 +4,11 @@
     "category": "Stock",
     "author": "initOS GmbH, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-tools",
-    "summary": """This module adds a warning popup message while you are validating picking or 
+    "summary": """This module adds a warning popup message while you are validating picking or
     manufacturing like "Please verify the Lot/Serial before validating, Product Name: 001
     (i.e. All products names with the Lot/Serial number that is going to be used)".
-	The popup warning has two buttons either validate with the Lot/Serial or cancel for change 
-	the Lot/Serial and you could change it manually""",
+    The popup warning has two buttons either validate with the Lot/Serial or cancel for change
+    the Lot/Serial and you could change it manually""",
     "depends": [
         "stock",
         "mrp",
