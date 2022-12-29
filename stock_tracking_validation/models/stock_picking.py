48b5c1b9a711f709680942dae76ce515a1194739
dsolanki <dhara.solanki@initos.com>
2022-12-29 16:56:32 +0530

    [MIG] stock_tracking_validation: Migration from v12 to v15

---
+++ stock_tracking_validation/models/stock_picking.py
@@ -0,0 +1,44 @@
+from odoo import models
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def open_stock_tracking_wizard(self):
+        move_line_has_lot = self.move_ids_without_package.filtered(lambda m : m.move_line_ids.mapped('lot_id'))
+        if not move_line_has_lot:
+            return self.button_validate()
+        action = self.env.ref('stock_tracking_validation.action_product_stock_validation').read()[0]
+        product_data = '''<table class="table table-sm w-50 table-bordered table-striped table-hover m-2"><tr>
+                            <thead class="class="thead-light"">
+                                <tr>
+                                    <th>Product Name</th>
+                                    <th>Lot/Serial</th>
+                                </tr>
+                            </thead>
+                            <tbody>'''
+        for produce_line in self.move_ids_without_package:
+            name = produce_line.product_id.name
+            if not produce_line.move_line_ids.mapped('lot_id'):
+                continue
+            lot_name = ''
+            for lot_line in produce_line.move_line_ids:
+                if len(produce_line.move_line_ids) == 1:
+                    lot_name = lot_line.lot_id.name
+                else:
+                    if lot_line == produce_line.move_line_ids[-1]:
+                        lot_name += lot_line.lot_id.name
+                    else:
+                        lot_name += lot_line.lot_id.name + ", "
+            product_data +="""
+                <tr>
+                    <td class="border">%s</td>
+                    <td>%s</td>
+                </tr>
+            """% (name, lot_name)
+        product_data += '''
+                        </tbody>
+                        </table>
+                        '''
+
+        action['context'] = {'default_product_data': product_data}
+        return action
