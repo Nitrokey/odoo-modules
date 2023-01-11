43226af00dd4d37bf798e203e39c2b8f66eced5a
dsolanki <dhara.solanki@initos.com>
2023-01-11 17:16:37 +0530

    [15.0][ADD] website_sale_stock_provisioning_time

---
+++ website_sale_stock_provisioning_time/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Website Sale Stock Provisioning Time",
+    "summary": "Display provisioning time for a product in shop online",
+    "version": "15.0.1.0.0",
+    "category": "Website",
+    "license": "AGPL-3",
+    # Author
+    "author": "Nitrokey GmbH," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-tools",
+    "depends": ["website_sale", "product"],
+    "data": [
+        "views/product_template_view.xml",
+        "views/website_sale_product.xml",
+    ],
+}
