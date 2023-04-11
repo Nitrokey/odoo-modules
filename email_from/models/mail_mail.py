d2224fb87a25c79a5e5a5e868f04b62a5a64016d
ahashaam <ali.hashaam@initos.com>
2023-04-11 15:47:32 +0200

    #33547 ast value error from email_from module is fixed

--- email_from/models/mail_mail.py
+++ email_from/models/mail_mail.py
@@ -9,8 +9,8 @@ class MailMail(models.Model):
     def create(self, values):
         new_mail = super(MailMail, self).create(values)
         email_from_ids = self.env['ir.config_parameter'].sudo().get_param('email_from_ids.email_from')
-        email_from_ids = ast.literal_eval(email_from_ids)
         if email_from_ids:
+            email_from_ids = ast.literal_eval(email_from_ids)
             for email_from in self.env['email.from'].search([('id', 'in', email_from_ids)]):
                 from_email_model_ids = email_from.from_email_model_ids.mapped('model')
                 if from_email_model_ids:
