98709f6005c7b73372d69d9c92156e583452889d
jans23 <jans23@users.noreply.github.com>
2023-07-07 08:32:26 +0200

    limit_to_employees fixed

--- limit_to_employees/__manifest__.py
+++ limit_to_employees/__manifest__.py
@@ -12,7 +12,7 @@ When users do the following actions in odoo, it only shows internal users contac
     "website": "https://github.com/OCA/server-tools",
     "version": "15.0.1.1.0",
     # any module necessary for this one to work correctly
-    "depends": ["helpdesk_mgmt", "account"],
+    "depends": ["account"],
     # always loaded
     "data": [
         "views/account_invoice.xml",
