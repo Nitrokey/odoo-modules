501262678bdb8fb4047fc3d7b16080260760f5e8
fkantelberg <florian.kantelberg@initos.com>
2023-11-29 09:11:40 +0100

    Correct the indentation etc. for pre-commit

--- abandoned_carts/static/src/js/relational_fields.js
+++ abandoned_carts/static/src/js/relational_fields.js
@@ -1,18 +1,19 @@
 odoo.define("abandoned_carts.relational_fields", function (require) {
-  "use strict";
+    "use strict";
 
-  var FieldOne2Many = require("web.relational_fields");
+    var FieldOne2Many = require("web.relational_fields");
 
-  FieldOne2Many.FieldOne2Many.include({
-    _render: function () {
-      if (
-        this.value &&
-        (this.value.model === "res.partner" || this.value.model === "sale.order") &&
-        this.value.type === "list"
-      ) {
-        this.value.addHasSelectors = true;
-      }
-      return this._super();
-    },
-  });
+    FieldOne2Many.FieldOne2Many.include({
+        _render: function () {
+            if (
+                this.value &&
+                (this.value.model === "res.partner" ||
+                    this.value.model === "sale.order") &&
+                this.value.type === "list"
+            ) {
+                this.value.addHasSelectors = true;
+            }
+            return this._super();
+        },
+    });
 });
