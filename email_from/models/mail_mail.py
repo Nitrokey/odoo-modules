1774a727c0a033875e0565e264512ee68ce03f95
Ruben Ortlam <ruben.ortlam@initos.com>
2024-02-29 08:56:13 +0100

    Streamline code, remove redundant parts

--- email_from/models/mail_mail.py
+++ email_from/models/mail_mail.py
@@ -8,23 +8,9 @@ _logger = logging.getLogger(__name__)
 class MailMail(models.Model):
     _inherit = "mail.mail"
 
-    def _map_records(self, email_froms):
-        # Get the models to keep without change
-        domain = [("action", "=", "keep")]
-        to_keep = email_froms.filtered_domain(domain)
-
-        # Prepare a mapping for the adjustments. Reverse the order to keep the
-        # highest priority record
-        to_adjust = {
-            model: rec
-            for rec in (email_froms - to_keep).sorted("sequence", True)
-            for model in rec.mapped("model_ids.model")
-        }
-
-        return to_keep, to_adjust
-
-    def _filter_records(self, recs):
-        return recs
+    def _unique_key_for_mail(self):
+        active_model = self.env.context.get("active_model")
+        return self.model or active_model
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -32,25 +18,33 @@ class MailMail(models.Model):
 
         email_froms = self.env.company.email_from_ids.sudo().filtered("active")
 
-        to_keep, to_adjust = self._map_records(email_froms)
+        # Get the models to keep without change
+        domain = [("action", "=", "keep")]
+        to_keep = email_froms.filtered_domain(domain)
+
+        # Prepare a mapping for the adjustments. Reverse the order to keep the
+        # highest priority record
+        to_adjust = {
+            rec._unique_key_for_model(model): rec
+            for rec in (email_froms - to_keep).sorted("sequence", True)
+            for model in rec.mapped("model_ids.model")
+        }
 
         # Default
         domain = [("action", "=", "set"), ("model_ids", "=", False)]
         default = email_froms.filtered_domain(domain)[:1]
 
-        to_keep = to_keep.mapped("model_ids.model")
+        to_keep = {
+            rec._unique_key_for_model(model)
+            for rec in to_keep
+            for model in rec.mapped("model_ids.model")
+        }
 
-        active_model = self.env.context.get("active_model")
-        # Apply the mapping of the adjustments
-        domain = [("model", "not in", to_keep)]
-        if active_model in to_keep:
-            domain.append(("model", "!=", False))
+        for mail in res:
+            key = mail._unique_key_for_mail()
 
-        for mail in self._filter_records(res.filtered_domain(domain)):
-            model = res.model or active_model
-
-            rec = to_adjust.get(model, default)
-            if rec:
+            rec = to_adjust.get(key, default)
+            if rec and key not in to_keep:
                 mail.write({"email_from": rec.email_from})
 
         return res
--- email_from_mass_mailing/models/email_from.py
+++ email_from_mass_mailing/models/email_from.py
@@ -5,3 +5,6 @@ class EmailFrom(models.Model):
     _inherit = "email.from"
 
     apply_to_mailing = fields.Boolean()
+
+    def _unique_key_for_model(self, model):
+        return self.apply_to_mailing, model
