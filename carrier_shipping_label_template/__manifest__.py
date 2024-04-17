b4b765cdec813cfee1dbb8a77312ce67cccb139d
jans23 <jans23@users.noreply.github.com>
2024-04-17 12:25:38 +0200

    fix

--- carrier_shipping_label_template/__manifest__.py
+++ carrier_shipping_label_template/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": "Print shipping label from print menu",
     "version": "15.0.3.1.5",
     "depends": [
-ups_delivery_carrier        "delivery_deutsche_post",
+        "delivery_deutsche_post",
     ],
     "data": ["views/report_shipping_label.xml"],
     "auto_install": False,
