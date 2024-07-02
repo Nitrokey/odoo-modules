4ea9d08a7c509e2226c2254b83821d80a04a7597
fkantelberg <florian.kantelberg@initos.com>
2024-07-02 11:33:49 +0200

    Trigger the automatic printing for shipping label reports because super is overwritten

---
+++ delivery_automatic_print/models/ir_actions_report.py
@@ -0,0 +1,25 @@
+# © 2024 initOS GmbH
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class IrActionsReport(models.Model):
+    _inherit = "ir.actions.report"
+
+    def _render_qweb_pdf(self, res_ids=None, data=None):
+        document, fmt = super()._render_qweb_pdf(res_ids, data)
+
+        if self.is_shipping_label_report():
+            # Implement the auto printing because super isn't called.
+            # See base_report_to_printer/models/ir_actions_report.py
+            behaviour = self.behaviour()
+            printer = behaviour.pop("printer", None)
+            can_print_report = self._can_print_report(behaviour, printer, document)
+
+            if can_print_report:
+                printer.print_document(
+                    self, document, doc_format=self.report_type, **behaviour
+                )
+
+        return document, fmt
