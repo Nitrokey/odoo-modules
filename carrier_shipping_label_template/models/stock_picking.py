abf677f0bcda96b6fca25756a20ace0d57cbbea8
fkantelberg <florian.kantelberg@initos.com>
2024-07-16 15:00:55 +0200

    [FIX] carrier_shipping_label_template: Don't copy shipping labels

--- carrier_shipping_label_template/models/stock_picking.py
+++ carrier_shipping_label_template/models/stock_picking.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    shipping_label_ids = fields.Many2many("ir.attachment", string="Labels")
+    shipping_label_ids = fields.Many2many("ir.attachment", string="Labels", copy=False)
