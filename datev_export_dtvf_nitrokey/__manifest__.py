f3b4412142d7bae9a1bc9ed5dfeb4409ea5d26dc
Holger Brunn <mail@hunki-enterprises.com>
2024-07-19 15:27:37 +0200

    [ADD] datev_export_dtvf_nitrokey

---
+++ datev_export_dtvf_nitrokey/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2024 Hunki Enterprises BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "DATEV, nitrokey customization",
+    "summary": "Export Data for DATEV (dtvf)",
+    "version": "15.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Accounting",
+    "author": "Hunki Enterprises BV",
+    "license": "AGPL-3",
+    "depends": [
+        "datev_export_dtvf",
+    ],
+}
---
+++ datev_export_dtvf_nitrokey/models/datev_export_dtvf.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Hunki Enterprises BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import models
+
+
+class DatevExportDtvfExport(models.Model):
+    _inherit = "datev_export_dtvf.export"
+
+
+    def _get_data_transaction(self, move):
+        """Try to export whatever looks like an SO number as "Belegfeld 1" """
+        for data in super()._get_data_transaction(move):
+            for line in move.line_ids:
+                for field_name in ('ref', 'name', 'move_name'):
+                    if (line[field_name] or '').startswith('SO'):
+                        data["Belegfeld 1"] = line[field_name]
+            yield data
