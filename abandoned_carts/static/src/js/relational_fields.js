c6eb58d6866b8db6e0eeae86ae7fe65d00e9f63e
Jan Suhr <jan@nitrokey.com>
2025-01-28 13:08:47 +0100

    fix "use strict"

--- abandoned_carts/static/src/js/relational_fields.js
+++ abandoned_carts/static/src/js/relational_fields.js
@@ -1,21 +1,19 @@
-(function () {
+odoo.define("abandoned_carts.relational_fields", function (require) {
     "use strict";
 
-    odoo.define("abandoned_carts.relational_fields", function (require) {
-        var FieldOne2Many = require("web.relational_fields");
+    var FieldOne2Many = require("web.relational_fields");
 
-        FieldOne2Many.FieldOne2Many.include({
-            _render: function () {
-                if (
-                    this.value &&
-                    (this.value.model === "res.partner" ||
-                        this.value.model === "sale.order") &&
-                    this.value.type === "list"
-                ) {
-                    this.value.addHasSelectors = true;
-                }
-                return this._super();
-            },
-        });
+    FieldOne2Many.FieldOne2Many.include({
+        _render: function () {
+            if (
+                this.value &&
+                (this.value.model === "res.partner" ||
+                    this.value.model === "sale.order") &&
+                this.value.type === "list"
+            ) {
+                this.value.addHasSelectors = true;
+            }
+            return this._super();
+        },
     });
-})();
+});
