e9611c2b35c48574e11d74ac9267acd854b53991
Jan Suhr <jan@nitrokey.com>
2025-04-14 15:09:01 +0200

    fix isort

--- product_icon/models/__init__.py
+++ product_icon/models/__init__.py
@@ -1,5 +1,4 @@
 ###############################################################################
 # For copyright and license notices, see __manifest__.py file in root directory
 ###############################################################################
-from . import product_icon
-from . import product_template
+from . import product_icon, product_template
