d9c346d750e15d5e5b76d40af50ad89d16534d8d
KDRdev <dmytro.kashuba@solvti.com>
2025-09-15 11:16:18 +0200

    Adding payment_bitcoin for V15 before module migration to V18

---
+++ payment_bitcoin/README.rst
@@ -0,0 +1,5 @@
+=========================
+Bitcoin Payment Acquirer
+=========================
+
+Payment Acquirer: Bitcoin Transfer Implementation
---
+++ payment_bitcoin/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    "name": "Bitcoin Payment Acquirer",
+    "category": "Hidden",
+    "summary": "Payment Acquirer: Bitcoin Transfer Implementation",
+    "version": "15.0.1.0.0",
+    "author": "Nitrokey GmbH, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-tools",
+    "license": "LGPL-3",
+    "depends": ["payment", "website_sale", "website_sale_payment", "base_automation"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/base_automation.xml",
+        "data/mail_data.xml",
+        "data/ir_cron_data.xml",
+        "views/bitcoin_views.xml",
+        "views/payment_bitcoin_templates.xml",
+        "data/payment_acquirer_data.xml",
+        "views/counter_qweb.xml",
+        "views/cart.xml",
+        "views/res_config_settings_view.xml",
+        "views/payment_acquirer.xml",
+        "views/sale_portal_view.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "payment_bitcoin/static/src/js/bitcoin.js",
+            "payment_bitcoin/static/src/js/countdown.js",
+            "payment_bitcoin/static/src/css/bitcoin_custom.css",
+        ],
+    },
+    "installable": True,
+    "auto_install": False,
+}
