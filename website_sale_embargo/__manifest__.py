9bfad7418da6c5a41e2ad895efd4265c40d6bd51
Jan Suhr <jan@nitrokey.com>
2025-10-10 23:44:43 +0200

    inital migration to 18.0

--- website_sale_embargo/__manifest__.py
+++ website_sale_embargo/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Website Sales Embargo",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "depends": [
         "product_harmonized_system",
         "sale",
--- website_sale_embargo/controllers/main.py
+++ website_sale_embargo/controllers/main.py
@@ -1,85 +1,80 @@
 from odoo import http
 from odoo.http import request
+from odoo.tools import str2bool
 
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
 class WebsiteSale(WebsiteSale):
-    @http.route(["/shop/confirm_order"], type="http", auth="public", website=True)
-    def confirm_order(self, **post):
+    @http.route(
+        ["/shop/confirm_order"], type="http", auth="public", website=True, sitemap=False
+    )
+    def shop_confirm_order(self, **post):
         order = request.website.sale_get_order()
         country_id = order.partner_shipping_id.country_id
         if country_id:
             order.check_for_product_embargo(country_id, True)
 
-        return super().confirm_order()
-
-    def get_mode(self, order, **kw):
-        Partner = request.env["res.partner"].with_context(show_address=1).sudo()
-        mode = (False, False)
-        partner_id = int(kw.get("partner_id", -1))
-
-        # IF PUBLIC ORDER
-        if order.partner_id.id == request.website.user_id.sudo().partner_id.id:
-            mode = ("new", "billing")
-        # IF ORDER LINKED TO A PARTNER
-        else:
-            if partner_id > 0:
-                if partner_id == order.partner_id.id:
-                    mode = ("edit", "billing")
-                else:
-                    shippings = Partner.search(
-                        [("id", "child_of", order.partner_id.commercial_partner_id.ids)]
-                    )
-                    if order.partner_id.commercial_partner_id.id == partner_id:
-                        mode = ("new", "shipping")
-                        partner_id = -1
-                    elif partner_id in shippings.mapped("id"):
-                        mode = ("edit", "shipping")
-            elif partner_id == -1:
-                mode = ("new", "shipping")
-        return mode
+        return super().shop_confirm_order(**post)
 
     @http.route(
         ["/shop/address"],
         type="http",
-        methods=["GET", "POST"],
+        methods=["GET"],
         auth="public",
         website=True,
         sitemap=False,
     )
-    def address(self, **kw):
+    def shop_address(
+        self,
+        partner_id=None,
+        address_type="billing",
+        use_delivery_as_billing=None,
+        **query_params
+    ):
         order = request.website.sale_get_order()
 
-        check_form_country = kw.get("country_id", False)
+        if redirection := self._check_cart(order):
+            return redirection
+
+        # Check if we're creating/editing a delivery address
+        check_form_country = query_params.get("country_id", False)
 
         if check_form_country:
             country_id = request.env["res.country"].browse(int(check_form_country))
         else:
             country_id = order.partner_shipping_id.country_id
 
-        mode = self.get_mode(order, **kw)
-
-        if country_id and mode[1] == "shipping":
+        # Only check embargo for delivery addresses
+        if country_id and address_type == "delivery":
             embargo_status = order.check_for_product_embargo(country_id)
             if embargo_status:
+                # Get the partner to edit, if any
+                partner_sudo, address_type = self._prepare_address_update(
+                    order, partner_id=partner_id and int(partner_id), address_type=address_type
+                )
+
+                # Get the base render values from parent
+                render_values = self._prepare_address_form_values(
+                    order,
+                    partner_sudo,
+                    address_type=address_type,
+                    use_delivery_as_billing=str2bool(use_delivery_as_billing or "false"),
+                    **query_params
+                )
+
+                # Add error information
                 errors = {
                     "product_embargo": "error",
                     "error_message": [embargo_status],
                 }
+                render_values["error"] = errors
 
-                render_values = {
-                    "website_sale_order": order,
-                    "partner_id": order.partner_id.id,
-                    "checkout": kw,
-                    "mode": mode,
-                    "error": errors,
-                    "callback": kw.get("callback"),
-                    "only_services": order and order.only_services,
-                }
-                render_values.update(
-                    self._get_country_related_render_values(kw, render_values)
-                )
                 return request.render("website_sale.address", render_values)
 
-        return super().address(**kw)
+        return super().shop_address(
+            partner_id=partner_id,
+            address_type=address_type,
+            use_delivery_as_billing=use_delivery_as_billing,
+            **query_params
+        )
