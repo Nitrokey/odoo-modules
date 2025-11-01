6363dfe012f272cd3b683c419337c6493eb62112
Jan Suhr <jan@nitrokey.com>
2025-11-01 19:45:29 +0100

    rebase with 18.0

--- payment_bitcoin/__init__.py
+++ payment_bitcoin/__init__.py
@@ -1,4 +1,1 @@
-###############################################################################
-# For copyright and license notices, see __manifest__.py file in root directory
-###############################################################################
-from . import models
+from . import controllers, models
