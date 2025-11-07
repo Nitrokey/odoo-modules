8d357a8ad15bf86f1bcf6f56faf04a8270ce9e04
jans23 <jans23@users.noreply.github.com>
2025-11-07 15:15:03 +0100

    Update author and website fields in manifest

--- website_menu_url_translate/__manifest__.py
+++ website_menu_url_translate/__manifest__.py
@@ -3,11 +3,8 @@
     "summary": "Translate website menu URLs for different languages.",
     "category": "Website",
     "version": "18.0.1.0.0",
-    "author": "GYB IT SOLUTIONS,"
-    "Nitrokey GmbH,"
-    "Odoo Community Association (OCA),"
-    "Synodica Solutions Pvt. Ltd.",
-    "website": "https://github.com/OCA/website",
+    "author": "GYB IT SOLUTIONS, Nitrokey GmbH, Synodica Solutions Pvt. Ltd.",
+    "website": "https://github.com/Nitrokey/odoo-modules/",
     "license": "LGPL-3",
     "depends": ["website"],
     "data": [
