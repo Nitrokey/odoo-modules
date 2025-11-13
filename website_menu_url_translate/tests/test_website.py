c34c5e193f796eb164c78f9a2cc8186645c81de6
hitesh-erpharbor <103484134+hitesh-erpharbor@users.noreply.github.com>
2025-11-13 16:48:36 +0530

    18.0 mig website menu url translate (#215)
    * [18.0][MIG] Migrated module website_menu_url_translate from v15 to v18
    
    * [ADD/IMP] Implemented the translation controller and Added the test data file and test .py file
    
    * Update author and website fields in manifest
    
    * [IMP/REM] Implemented the test py file and Remove test data xml file
    
    * [IMP] Implemented the formarting to pass the CI tests
    
    * [IMP] Implemented the formarting to pass the CI tests
    
    * [IMP] Implemented the readme file
    
    * fix README
    
    ---------
    
    Co-authored-by: jans23 <jans23@users.noreply.github.com>
    Co-authored-by: Jan Suhr <jan@nitrokey.com>

--- website_menu_url_translate/tests/test_website.py
+++ website_menu_url_translate/tests/test_website.py
@@ -1,4 +1,8 @@
+import logging
+
 from odoo.tests.common import HttpCase, tagged
+
+_logger = logging.getLogger(__name__)
 
 
 @tagged("post_install", "-at_install")
@@ -8,27 +12,46 @@ class TestWebsiteLangRedirect(HttpCase):
     def setUp(self):
         super().setUp()
         self.website = self.env["website"].get_current_website()
-        self.page_en = self.env.ref("website_menu_url_translate.test_contact_page_en")
-        self.page_de = self.env.ref("website_menu_url_translate.test_contact_page_de")
-        self.assertTrue(self.page_en.exists(), "English test page missing")
-        self.assertTrue(self.page_de.exists(), "German test page missing")
+        # Minimal QWeb view
+        self.view_contact = self.env["ir.ui.view"].create(
+            {
+                "name": "test_contactus_view",
+                "type": "qweb",
+                "arch_db": "<t t-name='test.contactus'>Contact Us Test Page</t>",
+            }
+        )
+        # Website page
+        self.page_en = self.env["website.page"].create(
+            {
+                "name": "Contact Us",
+                "url": "/test/contactus",
+                "view_id": self.view_contact.id,
+                "website_id": self.website.id,
+                "is_published": True,
+            }
+        )
+        # Translations
+        self.page_en.with_context(lang="en_US").url = "/test/contactus"
+        self.page_en.with_context(lang="de_DE").url = "/test/kontakt"
 
     def test_01_change_lang_redirect(self):
-        """Switch from English to German; expect redirect to /de/test/kontakt."""
+        """Test redirect to German version when language = de_DE."""
         resp = self.url_open(
             "/website/lang/de_DE?r=/test/contactus", allow_redirects=False
         )
         self.assertIn(resp.status_code, (303, 200))
+        redirect_url = resp.headers.get("Location")
+        _logger.info(
+            "[DE] Status: %s | Redirected to: %s", resp.status_code, redirect_url
+        )
 
     def test_02_default_language_redirect(self):
-        """Switch back to English; expect redirect to /test/contactus."""
+        """Test /website/lang redirection works cleanly."""
         resp = self.url_open(
-            "/website/lang/en_US?r=/test/kontakt", allow_redirects=False
+            "/website/lang/en_US?r=/test/contactus", allow_redirects=False
         )
         self.assertIn(resp.status_code, (303, 200))
-
-    def test_03_fallback_no_translation(self):
-        """If no translation exists, fallback to direct
-        url without languange /test/contactus."""
-        resp = self.url_open("/test/contactus", allow_redirects=False)
-        self.assertIn(resp.status_code, (303, 200))
+        redirect_url = resp.headers.get("Location")
+        _logger.info(
+            "[EN] Status: %s | Redirected to: %s", resp.status_code, redirect_url
+        )
