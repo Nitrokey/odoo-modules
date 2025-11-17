1f2d4f053256a9746c7ccc308311a8663512086f
HItesh Jadav <hitesh.j.erpharbor@gmail.com>
2025-11-17 18:52:22 +0530

    [18.0][MIG] Migrated the module mail_sound into v18

--- mail_sound/static/src/services/out_of_focus_service_patch.js
+++ mail_sound/static/src/services/out_of_focus_service_patch.js
@@ -1,7 +1,7 @@
 /** @odoo-module **/
 
-import {OutOfFocusService} from "@mail/core/common/out_of_focus_service";
-import {patch} from "@web/core/utils/patch";
+import { OutOfFocusService } from "@mail/core/common/out_of_focus_service";
+import { patch } from "@web/core/utils/patch";
 
 /**
  * Patch the OutOfFocusService to always play notification sounds
@@ -22,11 +22,13 @@ patch(OutOfFocusService.prototype, {
     async notify(message, thread) {
         // Call parent implementation to maintain standard notification behavior
         await super.notify(...arguments);
+        const currentUser = this.env.services["mail.store"].self;
+        const currentUserId = currentUser?.id;
 
         // Always play sound for messages from other users
         // The parent notify() already handles the focus-dependent behavior,
         // so we add unconditional sound playback here
-        if (message && message.author && message.author.id !== this.store.self.id) {
+        if (message && message.author && message.author.id !== currentUserId) {
             // Play the notification sound using the internal _playSound method
             // This ensures sound plays regardless of browser focus state
             this._playSound();
