3793ced693abc78be703219752d4bc20fc0b8e80
Jan Suhr <jan@nitrokey.com>
2025-11-18 12:10:32 +0100

    add account_invoice_send_on_reconcile

---
+++ account_invoice_send_on_reconcile/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Account Invoice Send on Reconcile",
+    "version": "15.0.1.0.0",
+    "category": "Accounting",
+    "author": "Nitrokey GmbH",
+    "license": "AGPL-3",
+    "website": "https://www.nitrokey.com",
+    "summary": "Automatically send invoice emails when reconciling bank payments",
+    "depends": [
+        "account",
+        "payment",
+    ],
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+}
