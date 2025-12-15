d59cadfa8a3d28bc660226c35fd848aae1a189ea
jans23 <jans23@users.noreply.github.com>
2025-12-15 09:05:57 +0100

    Update website and author information in manifest

--- mail_activity_board/__manifest__.py
+++ mail_activity_board/__manifest__.py
@@ -7,9 +7,8 @@
     "version": "18.0.1.1.0",
     "development_status": "Beta",
     "category": "Social Network",
-    "website": "https://github.com/OCA/mail",
-    "author": "SDi, David Juaneda, Sodexis, ACSONE SA/NV,"
-    " Odoo Community Association (OCA)",
+    "website": "https://github.com/nitrokey/odoo-modules",
+    "author": "SDi, David Juaneda, Sodexis, ACSONE SA/NV, Nitrokey",
     "license": "AGPL-3",
     "installable": True,
     "depends": ["calendar"],
