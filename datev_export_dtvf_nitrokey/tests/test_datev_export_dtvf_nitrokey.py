99232c324f5bacb3aafdd569034025287499be44
Holger Brunn <mail@hunki-enterprises.com>
2026-01-19 20:01:14 +0100

    [ADD] datev_export_dtvf_nitrokey (#237)
    * [ADD] datev_export_dtvf_nitrokey
    
    * Revise README and Manifest
    
    ---------
    
    Co-authored-by: jans23 <jans23@users.noreply.github.com>

---
+++ datev_export_dtvf_nitrokey/tests/test_datev_export_dtvf_nitrokey.py
@@ -0,0 +1,6 @@
+from odoo.addons.datev_export_dtvf.tests import test_datev_export_dtvf
+
+
+class TestDatevExportDtvfNitrokey(test_datev_export_dtvf.TestDatevExportDtvf):
+    def test_happy_flow(self):
+        return super().test_happy_flow()
