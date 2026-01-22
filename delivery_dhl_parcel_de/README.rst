07d4b82cb9b5fbf4038f14d8682381462c3b226b
Dmytro Kashuba @ Solvti <dmitry.kashuba.cv@gmail.com>
2026-01-22 13:14:36 +0100

    DHL Parcel DE Shipping Carrier (#233)
    * Module migration to 18.0 - initial version
    
    * Added package weight validation, CI fixes, README file
    
    * Pre-commit fixes
    
    * Small formatting fix
    
    * Pre-commit fix #2
    
    * README, manifest and dependencies update
    
    * OAuth2 implementation, merged newer module version from original author
    
    * Fix for multiple package types during weight limitation check. Added API secret field to view
    
    * Added neutralization script
    
    * pre-commit fix
    
    * README update
    
    * update description
    
    ---------
    
    Co-authored-by: Dmytro Kashuba <dmytro.kashuba@solvti.pl>
    Co-authored-by: jans23 <jans23@users.noreply.github.com>

---
+++ delivery_dhl_parcel_de/README.rst
@@ -0,0 +1,104 @@
+==============================================
+DHL Parcel (Post & Parcel Germany) Integration
+==============================================
+
+This module provides integration with DHL Parcel Germany (Post & Parcel Germany) for Odoo.
+
+Features
+--------
+* Create DHL Parcel DE shipments directly from Odoo
+* Update tracking information in Odoo from DHL
+* Generate and print shipping labels in Odoo
+
+Note that shipping rates are not retrieved from DHL but need to be configured.
+
+Configuration
+-------------
+
+Company
+^^^^^^^
+
+1. "Settings" -> "Users & Companies" -> "Companies"
+2. Select or create a company
+3. "DHL Parcel DE Configuration" tab -> set the "Use DHL Parcel DE Shipping Provider" as active
+4. Fill out the necessary information. The information below is only meant for testing purposes:
+
+   * DHL API URL: ``https://api-sandbox.dhl.com``
+   * DHL UserId: ``user-valid``
+   * DHL Password: ``SandboxPasswort2023!``
+   * DHL API Key: Add your own API key here
+   * DHL Tracking URL: (optional) Leave empty or use: ``https://www.dhl.de/en/privatkunden/pakete-empfangen/verfolgen.html?piececode=``
+
+5. Save your changes
+
+Create Delivery Methods
+^^^^^^^^^^^^^^^^^^^^^^^
+
+1. "Inventory" -> "Configuration" -> "Delivery" -> "Delivery Methods"
+2. Select or create a method
+3. Set the "Provider" to "DHL Parcel DE"
+4. Fill out the necessary information. The information below is only meant as an example:
+
+   * Company: Company Name
+   * DHL Weight UOM: ``KG``
+   * DHL Services Name: ``V53WPAK-DHL Paket``
+   * DHL Account number: ``3333333333`` (sandbox account)
+   * DHL Procedure number: ``01``
+   * DHL Participation number: ``02``
+   * DHL Package Info: Create package type
+
+     1. Click "Create"
+     2. Fill in package details:
+
+        * Package Type Name: ``DHL Parcel DE``
+        * Carrier Code: e.g., ``DHL-Paket``
+        * Height: e.g., ``5.00`` cm
+        * Width: e.g., ``5.00`` cm
+        * Length: e.g., ``5.00`` cm
+        * Weight: e.g., ``0.5`` kg
+        * Max Weight: e.g., ``10.00`` kg
+
+     3. Save your changes
+
+5. Save your changes
+
+
+Testing
+-------
+
+1. Create a Sales Order
+^^^^^^^^^^^^^^^^^^^^^^^
+
+1. "Sales" -> "Orders" -> "Quotations" -> "+New"
+2. Fill out the necessary information
+3. "Add Shipping" button -> set "Shipping Method" as "DHL Parcel DE" -> "Get Rate" button -> "Add" button -> "Confirm" button
+
+2. Create Delivery Order
+^^^^^^^^^^^^^^^^^^^^^^^^
+
+1. Click the "Delivery" button at the top of the panel in the sales order
+2. In the delivery order, set the shipping details:
+
+   * "Carrier" is set to your DHL Parcel DE method
+   * Ensure products have a weight configured
+
+3. "Validate" button -> set "Number Of Packages" to minimum 1 -> "Apply" button
+4. In the "Additional Info" tab, there should be a field called "Tracking Reference" (shows the shipment number for the product)
+5. At the top of the panel, look for the "Tracking" button (it will take you to the DHL tracking section)
+6. At the top of the panel, look for the "Valuation" button (it will display the minused quantities)
+
+
+Credits
+-------
+
+Authors
+^^^^^^^
+
+* Vraja Technologies
+* initOS GmbH
+* Nitrokey GmbH
+
+Contributors
+^^^^^^^^^^^^
+
+* Dmytro Kashuba <dmytro.kashuba@ext.initos.com>
---
+++ delivery_dhl_parcel_de/models/res_company.py
@@ -0,0 +1,100 @@
+import requests
+
+from odoo import fields, models
+from odoo.exceptions import ValidationError
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    use_dhl_parcel_de_shipping_provider = fields.Boolean(
+        copy=False,
+        string="Use DHL Parcel DE Shipping Provider",
+        help="If use DHL Parcel DE shipping provider than value set TRUE.",
+        default=False,
+    )
+    dhl_parcel_de_api_url = fields.Char(
+        string="DHL API URL", copy=False, default="https://api-sandbox.dhl.com"
+    )
+    dhl_userid = fields.Char(
+        "DHL UserId",
+        copy=False,
+        help="When use the sandbox account developer id use as the userId."
+        "When use the live account application id use as the userId.",
+    )
+    dhl_password = fields.Char(
+        "DHL Password",
+        copy=False,
+        help="When use the sandbox account developer portal password use "
+        "to as the password.When use the live account application "
+        "token use to as the password.",
+    )
+    dhl_api_key = fields.Char(
+        "DHL API Key",
+        copy=False,
+        help="Obtained via Get Access! (app creation) and manually approved by DHL.",
+    )
+    dhl_api_secret = fields.Char(
+        "DHL API Secret",
+        copy=False,
+        help="Obtained via Get Access! (app creation) and manually approved by DHL.",
+    )
+    dhl_tracking_url = fields.Char(
+        "DHL Tracking URL",
+        copy=False,
+        default="https://www.dhl.de/en/privatkunden/pakete-empfangen/verfolgen.html?piececode=",
+        help="Obtained via Get Access! (app creation) and manually approved by DHL.",
+    )
+    dhl_access_token = fields.Char(
+        "DHL Access Token",
+        copy=False,
+    )
+
+    def dhl_parcel_de_get_access_token(self):
+        """
+        This method is used to get access token from DHL Parcel DE Shipping Provider.
+        #OAuth2
+        """
+        url = f"{self.dhl_parcel_de_api_url}/parcel/de/account/auth/ropc/v1/token"
+
+        payload = {
+            "grant_type": "password",
+            "username": self.dhl_userid,
+            "password": self.dhl_password,
+            "client_id": self.dhl_api_key,
+            "client_secret": self.dhl_api_secret,
+        }
+
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+
+        try:
+            response = requests.request(
+                "POST", url, headers=headers, data=payload, timeout=30
+            )
+            if response.status_code in [200, 201]:
+                response_data = response.json()
+                if response_data["access_token"]:
+                    self.dhl_access_token = response_data["access_token"]
+                    return {
+                        "effect": {
+                            "fadeout": "slow",
+                            "message": "Yeah! DHL Token Retrieved successfully!!",
+                            "img_url": "/web/static/img/smile.svg",
+                            "type": "rainbow_man",
+                        }
+                    }
+                else:
+                    raise ValidationError(response_data)  # noqa: B904
+            else:
+                raise ValidationError(response.text)
+        except Exception as e:
+            raise ValidationError(e)  # noqa: B904
+
+    def dhl_parcel_de_get_access_token_cron(self):
+        for company_id in self.search(
+            [("use_dhl_parcel_de_shipping_provider", "=", True)]
+        ):
+            company_id.dhl_parcel_de_get_access_token()
