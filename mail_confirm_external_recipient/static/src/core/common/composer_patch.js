2a4a501e341f60eddbfcbbfaee44805dc8580849
Dmytro Kashuba <dmytro.kashuba@solvti.com>
2026-02-27 10:54:40 +0100

    pre-commit fixes

--- mail_confirm_external_recipient/static/src/core/common/composer_patch.js
+++ mail_confirm_external_recipient/static/src/core/common/composer_patch.js
@@ -1,8 +1,8 @@
 /** @odoo-module **/
 
-import {_t} from "@web/core/l10n/translation";
 import {Composer} from "@mail/core/common/composer";
 import {ConfirmationDialog} from "@web/core/confirmation_dialog/confirmation_dialog";
+import {_t} from "@web/core/l10n/translation";
 import {patch} from "@web/core/utils/patch";
 import {toRaw} from "@odoo/owl";
 
@@ -15,47 +15,7 @@ patch(Composer.prototype, {
             return superSendMessage.apply(this, arguments);
         }
 
-        const thread = toRaw(composer.thread);
-        const type = this.props.type; // 'message' or 'note'
-
-        const externalRecipients = [];
-
-        if (type !== "note" && thread && thread.model !== "discuss.channel") {
-            const followers = thread.followers || [];
-            for (const follower of followers) {
-                const partner = toRaw(follower.partner);
-                if (partner && !partner.isInternalUser) {
-                    const name = partner.name || partner.email;
-                    if (name && !externalRecipients.includes(name)) {
-                        externalRecipients.push(name);
-                    }
-                }
-            }
-        }
-
-        const mentionedPartners = composer.mentionedPartners || [];
-        for (const partner of mentionedPartners) {
-            if (!partner.isInternalUser) {
-                const name = partner.name || partner.email;
-                if (!externalRecipients.includes(name)) {
-                    externalRecipients.push(name);
-                }
-            }
-        }
-
-        if (type !== "note" && thread && thread.suggestedRecipients) {
-            for (const recipient of thread.suggestedRecipients) {
-                if (recipient.checked && recipient.persona) {
-                    const persona = this.store.Persona.get(recipient.persona);
-                    if (persona && !persona.isInternalUser) {
-                        const name = persona.name || persona.email || recipient.name;
-                        if (name && !externalRecipients.includes(name)) {
-                            externalRecipients.push(name);
-                        }
-                    }
-                }
-            }
-        }
+        const externalRecipients = this._getExternalRecipients();
 
         if (externalRecipients.length > 0) {
             const recipientNames = externalRecipients.join(", ");
@@ -80,5 +40,64 @@ patch(Composer.prototype, {
         }
 
         return superSendMessage.apply(this, arguments);
+    },
+
+    _getExternalRecipients() {
+        const externalRecipients = new Set();
+        this._addFollowerRecipients(externalRecipients);
+        this._addMentionedRecipients(externalRecipients);
+        this._addSuggestedRecipients(externalRecipients);
+        return [...externalRecipients];
+    },
+
+    _addFollowerRecipients(externalRecipients) {
+        const composer = toRaw(this.props.composer);
+        const thread = toRaw(composer.thread);
+        const type = this.props.type;
+        if (type === "note" || !thread || thread.model === "discuss.channel") {
+            return;
+        }
+        for (const follower of thread.followers || []) {
+            const partner = toRaw(follower.partner);
+            if (partner && !partner.isInternalUser) {
+                const name = partner.name || partner.email;
+                if (name) {
+                    externalRecipients.add(name);
+                }
+            }
+        }
+    },
+
+    _addMentionedRecipients(externalRecipients) {
+        const composer = toRaw(this.props.composer);
+        for (const partner of composer.mentionedPartners || []) {
+            if (!partner.isInternalUser) {
+                const name = partner.name || partner.email;
+                if (name) {
+                    externalRecipients.add(name);
+                }
+            }
+        }
+    },
+
+    _addSuggestedRecipients(externalRecipients) {
+        const composer = toRaw(this.props.composer);
+        const thread = toRaw(composer.thread);
+        const type = this.props.type;
+        if (type === "note" || !thread || !thread.suggestedRecipients) {
+            return;
+        }
+        for (const recipient of thread.suggestedRecipients) {
+            if (!recipient.checked || !recipient.persona) {
+                continue;
+            }
+            const persona = this.store.Persona.get(recipient.persona);
+            if (persona && !persona.isInternalUser) {
+                const name = persona.name || persona.email || recipient.name;
+                if (name) {
+                    externalRecipients.add(name);
+                }
+            }
+        }
     },
 });
