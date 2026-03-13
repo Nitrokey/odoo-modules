981d162f060242172239d924e0f2dfa222200579
Hitesh Jadav <103484134+hitesh-erpharbor@users.noreply.github.com>
2026-03-13 14:21:35 +0530

    [18.0][MIG] partner_user: Migration to v18.0 (#257)
    * [18.0][MIG] partner_user: Migration to v18.0
    
    * [18.0][MIG] partner_user: Migration to v18.0
    
    * remove OCA, descriptions
    
    ---------
    
    Co-authored-by: Jan Suhr <jan@nitrokey.com>

---
+++ partner_user/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Partner User",
+    "version": "18.0.1.0.0",
+    "summary": """Added Users field in Partner form view""",
+    "category": "Tools",
+    "author": "Nitrokey GmbH",
+    "website": "https://github.com/Nitrokey/odoo-modules",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [
+        "view/partner.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
