f812ad90c6be5dc619d77fe32411c603a8ca83b0
Hitesh Jadav <hitesh.j.erpharbor@gmail.com>
2026-03-16 16:58:03 +0530

    [IMP] Rechanged the Mail Sound Notifications version

--- mail_sound/__manifest__.py
+++ mail_sound/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Mail Sound Notifications",
-    "version": "18.0.1.0.20",
+    "version": "18.0.1.0.0",
     "category": "Discuss",
     "summary": "Play sound when receiving messages",
     "author": "Nitrokey GmbH",
