c7c00327642b369d7e53997c08b5910d8bb55cb9
jans23 <jans23@users.noreply.github.com>
2026-05-18 14:48:46 +0200

    avoid automatic printing of invoices (#272)

--- datev_export_xml_nitrokey/README.rst
+++ datev_export_xml_nitrokey/README.rst
@@ -4,3 +4,4 @@ DATEV Export XML Nitrokey customization
 - DATEV export will contain sales order number instead of invoice number
 - check for some UTF8 ranges before export
 - suppress lines with a total of 0 entirely
+- avoid automatic printing of invoices on paper (via CUPS)
---
+++ datev_export_xml_nitrokey/models/datev_pdf_generator.py
@@ -0,0 +1,14 @@
+# © 2023 initOS GmbH
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class DatevPdfGenerator(models.AbstractModel):
+    _inherit = "datev.pdf.generator"
+
+    @api.model
+    def generate_pdf(self, invoice):
+        return super(
+            DatevPdfGenerator, self.with_context(must_skip_send_to_printer=True)
+        ).generate_pdf(invoice)
