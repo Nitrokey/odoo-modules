c5266656843cfb6cfc6e52f5ff673b9595a557ae
Jan Suhr <jan@nitrokey.com>
2026-07-01 13:07:54 +0200

    add base_customs_territory

---
+++ base_customs_territory/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Base Customs Territory",
+    "version": "18.0.1.0.0",
+    "category": "Base",
+    "summary": "Flag states/regions outside the customs territory of their country",
+    "license": "AGPL-3",
+    "author": "Nitrokey",
+    "website": "https://github.com/Nitrokey/odoo-modules/",
+    "maintainer": ["initOS", "Nitrokey"],
+    "depends": ["base"],
+    "data": [
+        "data/res_country_state_data.xml",
+    ],
+    "installable": True,
+}
